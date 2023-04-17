0401cd0f3f7cb5227b84f3acdf1b3670c70d86ea
jkorzeniowski <jkorzeniowski21@gmail.com>
2023-04-17 10:42:54 +0200

    feat: creating class, putting all users tests to the class, removing unnecessary assertions

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,87 +2,77 @@ import requests
 from variables import ENDPOINT, REQUEST_TIMEOUT_GLOBAL
 
 
-def test_get_user_list():
-    response = requests.get(f"{ENDPOINT}/api/users", timeout=REQUEST_TIMEOUT_GLOBAL)
-    assert response.status_code == 200, f"Something went wrong, actual status code: {response.status_code}\n" \
-                                        f"while expected code: 200"
+class TestUsers:
+    """
+    API tests regarding user management.
+    """
 
+    def test_get_user_list(self):
+        response = requests.get(f"{ENDPOINT}/api/users", timeout=REQUEST_TIMEOUT_GLOBAL)
+        assert response.status_code == 200, f"Something went wrong, actual status code: {response.status_code}\n" \
+                                            f"while expected code: 200"
 
-def test_get_user_data_section():
-    expected_first_user = {
-        'id': 1, 'email': 'george.bluth@reqres.in', 'first_name': 'George',
-        'last_name': 'Bluth', 'avatar': 'https://reqres.in/img/faces/1-image.jpg'
-    }
-    response = requests.get(f"{ENDPOINT}/api/users/1", timeout=REQUEST_TIMEOUT_GLOBAL)
-    assert response.status_code == 200, f"Something went wrong, actual status code: {response.status_code}\n" \
-                                        f"while expected code: 200"
-    assert response.json(), "No JSON in the response"
-    assert response.json()['data'] == expected_first_user, "First user data differs. User data updated/changed.\n" \
-                                                           f"Expected data: {expected_first_user}\n" \
-                                                           f"Actual data: {response.json()['data']}"
+    def test_get_user_data_section(self):
+        expected_first_user = {
+            'id': 1, 'email': 'george.bluth@reqres.in', 'first_name': 'George',
+            'last_name': 'Bluth', 'avatar': 'https://reqres.in/img/faces/1-image.jpg'
+        }
+        response = requests.get(f"{ENDPOINT}/api/users/1", timeout=REQUEST_TIMEOUT_GLOBAL)
+        assert response.status_code == 200, f"Something went wrong, actual status code: {response.status_code}\n" \
+                                            f"while expected code: 200"
+        assert response.json(), "No JSON in the response"
+        assert response.json()['data'] == expected_first_user, "First user data differs. User data updated/changed.\n" \
+                                                               f"Expected data: {expected_first_user}\n" \
+                                                               f"Actual data: {response.json()['data']}"
 
+    def test_get_user_support_section(self):
+        response = requests.get(f"{ENDPOINT}/api/users/1", timeout=REQUEST_TIMEOUT_GLOBAL)
+        assert response.status_code == 200, f"Something went wrong, actual status code: {response.status_code}\n" \
+                                            f"while expected code: 200"
+        assert response.json(), "No JSON in the response"
 
-def test_get_user_support_section():
-    expected_support_section = {
-        'url': 'https://reqres.in/#support-heading',
-        'text': 'To keep ReqRes free, contributions towards server costs are appreciated!'
-    }
+    def test_get_invalid_user(self):
+        response = requests.get(f"{ENDPOINT}/api/users/999", timeout=REQUEST_TIMEOUT_GLOBAL)
+        assert response.status_code == 404, f"Something went wrong, " \
+                                            f"expected code: 404, while actual code was {response.status_code}"
 
-    response = requests.get(f"{ENDPOINT}/api/users/1", timeout=REQUEST_TIMEOUT_GLOBAL)
-    assert response.status_code == 200, f"Something went wrong, actual status code: {response.status_code}\n" \
-                                        f"while expected code: 200"
-    assert response.json(), "No JSON in the response"
+        assert not response.json(), "JSON was available in the response but not expected any."
 
-    assert response.json()['support'] == expected_support_section, \
-        "Help section differs. Help section updated/changed.\n" \
-        f"Expected data: {expected_support_section}\n" \
-        f"Actual data: {response.json()['support']}"
+    def test_create_and_delete_user(self):
+        user_data = {
+            "name": "Karim",
+            "job": "Scientist"
+        }
+        create_response = requests.post(f"{ENDPOINT}/api/users", json=user_data, timeout=REQUEST_TIMEOUT_GLOBAL)
+        create_response_data = create_response.json()
+        assert create_response.status_code == 201, f"Something went wrong, actual status code: " \
+                                                   f"{create_response.status_code}\n" \
+                                                   f"while expected code: 201"
+        assert create_response.json(), "Response have no response data (JSON) or response data is invalid"
+        assert create_response_data['name'] == user_data['name'], f"Data in response differs.\n" \
+                                                                  f"User name in response: {create_response_data['name']}" \
+                                                                  f"User name provided: {user_data['name']}"
+        assert create_response_data['job'] == user_data['job'], f"Data in response differs.\n" \
+                                                                f"User job in response: {create_response_data['job']}" \
+                                                                f"User job provided: {user_data['job']}"
+        assert create_response_data['id'], "ID for the user not set or not provided in response."
+        assert create_response_data['id'].isnumeric(), \
+            f"ID for the user is not integer type: ID: {create_response_data['id']}"
 
+        delete_response = requests.delete(f"{ENDPOINT}/api/users/{create_response_data['id']}",
+                                          timeout=REQUEST_TIMEOUT_GLOBAL)
+        assert delete_response.status_code == 204, f"Something went wrong, actual status code: " \
+                                                   f"{delete_response.status_code}\n" \
+                                                   f"while expected code: 204"
 
-def test_get_invalid_user():
-    response = requests.get(f"{ENDPOINT}/api/users/999", timeout=REQUEST_TIMEOUT_GLOBAL)
-    assert response.status_code == 404, f"Something went wrong, " \
-                                        f"expected code: 404, while actual code was {response.status_code}"
+    def test_update_user(self, user_setup):
+        new_user_data = {
+            "name": "Tom",
+            "job": "Actor"
+        }
+        response = requests.patch(f"{ENDPOINT}/api/users/{user_setup['id']}", json=new_user_data,
+                                  timeout=REQUEST_TIMEOUT_GLOBAL)
 
-    assert not response.json(), "JSON was available in the response but not expected any"
-
-
-def test_create_and_delete_user():
-    user_data = {
-        "name": "Karim",
-        "job": "Scientist"
-    }
-    create_response = requests.post(f"{ENDPOINT}/api/users", json=user_data, timeout=REQUEST_TIMEOUT_GLOBAL)
-    assert create_response.status_code == 201, f"Something went wrong, actual status code: " \
-                                               f"{create_response.status_code}\n" \
-                                               f"while expected code: 201"
-    assert create_response.json(), "No JSON in the response"
-
-    response_data = create_response.json()
-    assert response_data['name'] == user_data['name'], f"Data in response differs.\n" \
-                                                       f"User name in response: {response_data['name']}" \
-                                                       f"User name provided: {user_data['name']}"
-    assert response_data['job'] == user_data['job'], f"Data in response differs.\n" \
-                                                     f"User job in response: {response_data['job']}" \
-                                                     f"User job provided: {user_data['job']}"
-    assert response_data['id'], "ID for the user not set or not provided in response."
-    assert response_data['id'].isnumeric(), f"ID for the user is not integer type: ID: {response_data['id']}"
-    assert response_data['createdAt'], "Missing 'createdAt' field from the response."
-
-    delete_response = requests.delete(f"{ENDPOINT}/api/users/{response_data['id']}", timeout=REQUEST_TIMEOUT_GLOBAL)
-    assert delete_response.status_code == 204, f"Something went wrong, actual status code: " \
-                                               f"{delete_response.status_code}\n" \
-                                               f"while expected code: 204"
-
-
-def test_update_user(user_setup):
-    new_user_data = {
-        "name": "Tom",
-        "job": "Actor"
-    }
-    response = requests.patch(f"{ENDPOINT}/api/users/{user_setup['id']}", json=new_user_data,
-                              timeout=REQUEST_TIMEOUT_GLOBAL)
-
-    assert response.status_code == 200, f"Something went wrong, actual status code: {response.status_code}\n" \
-                                        f"while expected code: 200"
-    assert response.json() and response.json()["updatedAt"], "Response have no response or response data is invalid"
+        assert response.status_code == 200, f"Something went wrong, actual status code: {response.status_code}\n" \
+                                            f"while expected code: 200"
+        assert response.json(), "Response have no response data (JSON) or response data is invalid"
